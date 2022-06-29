5780c5116878821a088a66d0ea4db97a635a1587
jjmferreira <72138111+jjmferreira@users.noreply.github.com>
2022-06-29 16:33:30 +0100

    white rec and text

---
+++ ivml/src/App.js
@@ -0,0 +1,12 @@
+import Dados from "./components/Dados";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> App Start</h1>
+      <Dados title='teste'/>
+    </div>
+  );
+}
+
+export default App;
